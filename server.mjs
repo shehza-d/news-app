ba1a397033424da82d6c503739e64e420911f8d0
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-12 13:30:30 +0500

    fixed

--- server.mjs
+++ server.mjs
@@ -1,12 +1,21 @@
 import express, { response } from "express";
-console.log(`Im sever file from Shehzad`)
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.get('/',(request, response)=>{
 	response.send("Server hello world shehzad")
+	console.log(`Im sever file from Shehzad`)
 })
+app.get('/sd',(request, response)=>{
+	response.send("testing / routes")
+	console.log(`testing / routes`)
+})
+app.post('/sd',(request, response)=>{
+	response.send("testing / routes with post")
+	console.log(`testing / routes with post`)
+})
+
 
 app.listen(PORT, () => {
 	console.log(`Examples app listening on port ${PORT}`)
