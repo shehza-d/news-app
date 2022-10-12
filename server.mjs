05560274395108a870fe44ead00f8aeb611469f4
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-12 15:18:30 +0500

    push

--- server.mjs
+++ server.mjs
@@ -3,20 +3,40 @@ import express, { response } from "express";
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-app.get('/',(request, response)=>{
-	response.send("Server hello world shehzad")
-	console.log(`Im sever file from Shehzad`)
-})
-app.get('/sd',(request, response)=>{
-	response.send("testing / routes")
-	console.log(`testing / routes`)
-})
-app.post('/sd',(request, response)=>{
-	response.send("testing / routes with post")
-	console.log(`testing / routes with post`)
-})
+let todos = [];
 
+app.use(express.json()); 
+
+app.post("/todo", (request, response) => {
+  todos.push(request.body.text);
+  response.send({
+    message: "your data is saved",
+    data: todos
+  });
+});
+
+app.get("/todos", (request, response) => {
+  response.send({
+    message: "here is your todo list",
+    data: todos,
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+// app.get("/sd", (request, response) => {
+//   response.send("testing / routes");
+//   console.log(`testing / routes`);
+// });
 
 app.listen(PORT, () => {
-	console.log(`Examples app listening on port ${PORT}`)
-  })
+  console.log(`Examples app listening on port ${PORT}`);
+});
