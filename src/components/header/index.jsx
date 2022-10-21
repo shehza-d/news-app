00dc92341ce64749e227e9c2217b26394f941f21
Shehzad <shehzaddiqbal@gmail.com>
2022-10-21 23:06:52 +0500

    Pushing new Changes

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,10 @@
+import moment from 'moment';
 import './index.css'
+
 const Header = () => {
     return (
         <div className="header">
+       {/*<nav class="nav-bar">*/}
             <h1>
                 News App
             </h1>
@@ -9,7 +12,15 @@ const Header = () => {
                 <li>Home</li>
                 <li>About</li>
             </ul>
-
+            <div class="dateDiv">
+                <div>
+                    <div id="date">{moment().format(
+    "D MMM YYYY"
+  )}</div>
+                    <div id="time">{ moment().format("h:mm:s a")}</div>
+                </div>
+                <button>O</button>
+            </div>
         </div>
     );
 };
